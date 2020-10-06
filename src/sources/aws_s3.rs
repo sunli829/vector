b1a673a4dbd3d9a5e0e1a6cf3994ae2d9ef8072d
Jesse Szwedko <jesse@szwedko.me>
2020-10-06 18:06:57 -0400

    wip

--- src/sources/aws_s3.rs
+++ src/sources/aws_s3.rs
@@ -1,28 +1,41 @@
-jse crate::{
+use crate::{
     config::{DataType, GlobalOptions, SourceConfig, SourceDescription},
     dns::Resolver,
     event::Event,
+    region::RegionOrEndpoint,
     shutdown::ShutdownSignal,
     sinks::util::rusoto,
     Pipeline,
 };
+use codec::BytesDelimitedCodec;
 use futures::{
-    compat::Future01CompatExt,
+    compat::{Compat, Future01CompatExt},
     future::{FutureExt, TryFutureExt},
     stream::StreamExt,
 };
 use futures01::Sink;
 use rusoto_core::Region;
-use rusoto_s3::{S3Client, S3};
+use rusoto_s3::{GetObjectRequest, S3Client, S3};
 use rusoto_sqs::{GetQueueUrlRequest, ReceiveMessageRequest, Sqs, SqsClient};
 use serde::{Deserialize, Serialize};
-use std::time::Duration;
+use std::{convert::TryInto, time::Duration};
 use tokio::{select, time};
+use tokio_util::codec::FramedRead;
 
 // TODO:
 // * Revisit configuration of queue. Should we take the URL instead?
 //   * At the least, support setting a differente queue owner
 // * Move AWS utils from sink to general
+// * Use multiline config
+// * Share provider config?
+//
+// * Look at Tower service
+//
+// Questions:
+// * Handling shutdown gracefully
+//   * Don't poll for new messages; finish processing current set
+// * Concurrency. Should we process objects / messages concurrently?
+// * Why does send_all/forward return the sink?
 
 #[derive(Copy, Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "lowercase")]
@@ -54,6 +67,7 @@ impl Default for Strategy {
 }
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
 struct AwsS3Config {
     #[serde(default)]
     compression: Compression,
@@ -71,7 +85,8 @@ struct AwsS3Config {
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(deny_unknown_fields)]
 struct SqsConfig {
-    region: Region,
+    #[serde(flatten)]
+    region: RegionOrEndpoint,
     queue_name: String,
     #[serde(default = "default_poll_interval_secs")]
     poll_secs: u64,
@@ -107,16 +122,13 @@ impl SourceConfig for AwsS3Config {
         out: Pipeline,
     ) -> crate::Result<super::Source> {
         match self.strategy {
-            Strategy::Sqs => match self.sqs {
-                Some(ref sqs) => Ok(Box::new(
-                    SqsIngestor::new(sqs.clone(), self.assume_role.clone(), self.compression)
-                        .await
-                        .run(out, shutdown)
-                        .boxed()
-                        .compat(),
-                )),
-                None => unimplemented!("TODO"),
-            },
+            Strategy::Sqs => Ok(Box::new(
+                self.create_sqs_ingestor()
+                    .await
+                    .run(out, shutdown)
+                    .boxed()
+                    .compat(),
+            )),
         }
     }
 
@@ -129,10 +141,33 @@ impl SourceConfig for AwsS3Config {
     }
 }
 
-struct SqsIngestor {
-    sqs_client: SqsClient,
+impl AwsS3Config {
+    async fn create_sqs_ingestor(&self) -> SqsIngestor {
+        match self.sqs {
+            Some(ref sqs) => {
+                let region: Region = (&sqs.region).try_into().unwrap();
+                // TODO:
+                // * move resolver?
+                // * try cloning credentials provider again?
+                let resolver = Resolver;
+                let client = rusoto::client(resolver).unwrap();
+                let creds =
+                    rusoto::AwsCredentialsProvider::new(&region, self.assume_role.clone()).unwrap();
+                let sqs_client = SqsClient::new_with(client.clone(), creds, region.clone());
+                let creds =
+                    rusoto::AwsCredentialsProvider::new(&region, self.assume_role.clone()).unwrap();
+                let s3_client = S3Client::new_with(client.clone(), creds, region.clone());
 
-    assume_role: Option<String>,
+                SqsIngestor::new(sqs_client, s3_client, sqs.clone(), self.compression).await
+            }
+            None => unimplemented!("TODO"),
+        }
+    }
+}
+
+struct SqsIngestor {
+    s3_client: S3Client,
+    sqs_client: SqsClient,
 
     compression: Compression,
 
@@ -144,18 +179,12 @@ struct SqsIngestor {
 
 impl SqsIngestor {
     async fn new(
+        sqs_client: SqsClient,
+        s3_client: S3Client,
         config: SqsConfig,
-        assume_role: Option<String>,
         compression: Compression,
     ) -> SqsIngestor {
         // TODO error handling
-        // TODO move resolver?
-        let resolver = Resolver;
-        let client = rusoto::client(resolver).unwrap();
-        let creds =
-            rusoto::AwsCredentialsProvider::new(&config.region, assume_role.clone()).unwrap();
-        let sqs_client = SqsClient::new_with(client, creds, config.region.clone());
-
         let queue_url = sqs_client
             .get_queue_url(GetQueueUrlRequest {
                 queue_name: config.queue_name.clone(),
@@ -167,9 +196,8 @@ impl SqsIngestor {
             .unwrap();
 
         SqsIngestor {
+            s3_client,
             sqs_client,
-
-            assume_role,
 
             compression,
 
@@ -180,9 +208,11 @@ impl SqsIngestor {
         }
     }
 
-    async fn run(self, mut out: Pipeline, shutdown: ShutdownSignal) -> Result<(), ()> {
+    async fn run(self, out: Pipeline, shutdown: ShutdownSignal) -> Result<(), ()> {
         let mut interval = time::interval(self.poll_interval).map(|_| ());
         let mut shutdown = shutdown.compat();
+        let queue_url = &self.queue_url;
+        let sqs_client = &self.sqs_client;
 
         loop {
             select! {
@@ -191,52 +221,136 @@ impl SqsIngestor {
                 else => break,
             };
 
-            let events = self.poll_events().await;
-
-            // Poll for messages
-            // For each message:
-            //   * Parse
-            //   * Forward logs
-            //   * Delete message (if delete_message=true)
-
-            let (sink, _) = out
-                .send_all(futures01::stream::iter_ok(logs))
-                .compat()
+            let receive_message_result = sqs_client
+                .receive_message(ReceiveMessageRequest {
+                    // TODO additional parameters?
+                    // TODO evaluate wait_time_seconds
+                    queue_url: queue_url.to_string(),
+                    max_number_of_messages: Some(10),
+                    wait_time_seconds: Some(10),
+                    ..Default::default()
+                })
                 .await
-                .map_err(|error| error!(message = "Error sending S3 Logs", %error))?;
-            out = sink;
+                .unwrap();
+
+            //let messages: Box<dyn Stream<Item = (Bytes, String), Error = ()> + Send> =
+            for message in receive_message_result.messages.unwrap_or_default() {
+                // TODO
+                // * Handle empty messages
+                //
+                // parse message
+                // handle_message (process S3 objects)
+                // ack message
+                let s3_event: S3Event =
+                    serde_json::from_str(message.body.unwrap_or_default().as_ref()).unwrap();
+
+                match self.handle_s3_event(s3_event, out.clone()).await {
+                    Ok(()) => {}  // TODO ack message
+                    Err(()) => {} // TODO emit error
+                }
+            }
+        }
+        Ok(())
+    }
+    //let events = self.poll_events().await;
+
+    // Poll for messages
+    // For each message:
+    //   * Parse
+    //   * Forward logs
+    //   * Delete message (if delete_message=true)
+
+    //let (sink, _) = out
+    //.send_all(futures01::stream::iter_ok(logs))
+    //.compat()
+    //.await
+    //.map_err(|error| error!(message = "Error sending S3 Logs", %error))?;
+    //out = sink;
+    //
+    async fn handle_s3_event(&self, s3_event: S3Event, out: Pipeline) -> Result<(), ()> {
+        for record in s3_event.records {
+            match self.handle_s3_event_record(record, out.clone()).await {
+                Ok(()) => {}
+                Err(()) => return Err(()), // TODO emit error
+            }
         }
 
         Ok(())
     }
 
-    async fn capture_logs(&self) -> impl Iterator<Item = Event> {
-        self.sqs_client
-            .receive_message(ReceiveMessageRequest {
-                // TODO additional parameters?
-                // TODO evaluate wait_time_seconds
-                queue_url: self.queue_url.clone(),
-                max_number_of_messages: Some(10),
-                wait_time_seconds: Some(10),
+    // TODO handle shutdown
+    async fn handle_s3_event_record(
+        &self,
+        s3_event: S3EventRecord,
+        out: Pipeline,
+    ) -> Result<(), ()> {
+        let object = self
+            .s3_client
+            .get_object(GetObjectRequest {
+                bucket: s3_event.s3.bucket.name,
+                key: s3_event.s3.object.key,
                 ..Default::default()
             })
             .await
             .unwrap();
-        vec![].into_iter()
+
+        // TODO assert event type
+
+        match object.body {
+            Some(body) => {
+                // .filter_map(|r| )
+                // TODO: decompress
+                // TODO: other codecs
+                let stream = FramedRead::new(
+                    body.into_async_read(),
+                    BytesDelimitedCodec::new_with_max_length(b'\n', 100000),
+                )
+                .filter_map(|line| async move {
+                    match line {
+                        Ok(line) => Some(Ok(Event::from(line))),
+                        Err(err) => {
+                            // TODO handling IO errors here?
+                            dbg!(err);
+                            None
+                        }
+                    }
+                });
+
+                out.send_all(Compat::new(Box::pin(stream)))
+                    .compat()
+                    .await
+                    .map_err(|error| {
+                        error!(message = "Error sending S3 Logs", %error);
+                        ()
+                    })
+                    .map(|_| ())
+            }
+            None => Ok(()),
+        }
         // TODO:
         // Fetch SQS message(s)
         // Fetch object for each message
         // Decompress (if needed)
         // Create LogEvent, add tags
+        //async fn capture_logs(&self) -> impl Iterator<Item = Event> {
+        //vec![].into_iter()
+        //}
     }
+}
+
+// https://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+struct S3Event {
+    records: Vec<S3EventRecord>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
-struct Message {
+struct S3EventRecord {
     event_version: String, // TODO compare >=
     event_source: String,
-    aws_region: Region,
+    aws_region: String, // TODO validate?
     event_name: String, // TODO break up?
 
     s3: S3Message,
